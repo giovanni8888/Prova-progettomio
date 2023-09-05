e0d523bdf32b1b852f4e0ea4a62c953bcba0b8db
giovanni8888 <141340573+giovanni8888@users.noreply.github.com>
2023-09-05 15:37:45 +0200

    braccio semi funzionante

---
+++ My project/Assets/PlaceObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+using UnityEngine.InputSystem;
+
+public class PlaceObject : MonoBehaviour
+{
+    public GameObject ObjectToPlace;
+    private MagicLeapInputs.ControllerActions controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
